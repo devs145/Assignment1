3189c06830c2e2fa9359755749ff6048ae77a3f2
devs145 <sahildevnani145@gmail.com>
2022-10-26 13:18:10 -0600

    added outdoor pages

--- Getactive/www/js/scripts.js
+++ Getactive/www/js/scripts.js
@@ -12,10 +12,6 @@ var app = new Framework7({
       url: 'pages/categories.html',
     },
     {
-      path: '/indoor1/',
-      url: 'pages/indoor1.html',
-    },
-    {
       path: '/test1/',
       content: `
         <div class="page">
@@ -70,6 +66,63 @@ var app = new Framework7({
       `,
     },
 
+    {
+      path: '/test4/',
+      content: `
+        <div class="page">
+        <div class="toolbar tabbar toolbar-bottom no-shadow">
+          <div class="toolbar-inner">
+            <a href="/#/" class="link" id="home"><i class="icon material-icons">home</i></a>
+            <a href="/categories/" class="link" id="category"><i class="icon material-icons">category</i></a>
+          </div>
+        </div>
+          <div class="page-content">
+            <div class="block">
+              <p>Test4</p>
+            </div>
+          </div>
+        </div>
+      `,
+    },
+    {
+      path: '/test5/',
+      content: `
+        <div class="page">
+        <div class="toolbar tabbar toolbar-bottom no-shadow">
+          <div class="toolbar-inner">
+            <a href="/#/" class="link" id="home"><i class="icon material-icons">home</i></a>
+            <a href="/categories/" class="link" id="category"><i class="icon material-icons">category</i></a>
+          </div>
+        </div>
+          <div class="page-content">
+            <div class="block">
+              <p>Test5</p>
+            </div>
+          </div>
+        </div>
+      `,
+    },
+    {
+      path: '/test6/',
+      content: `
+        <div class="page">
+        <div class="toolbar tabbar toolbar-bottom no-shadow">
+          <div class="toolbar-inner">
+            <a href="/#/" class="link" id="home"><i class="icon material-icons">home</i></a>
+            <a href="/categories/" class="link" id="category"><i class="icon material-icons">category</i></a>
+          </div>
+        </div>
+          <div class="page-content">
+            <div class="block">
+              <p>Test6</p>
+            </div>
+          </div>
+        </div>
+      `,
+    },
+
+
+
   ],
 });
 var mainView = app.views.create('.view-main')
@@ -78,11 +131,21 @@ $(document).on('page:init',function(e) {
 
 $("#test").on("click", function() {
   var indoor = ["/test1/", "/test2/", "/test3/"]
-  var indexnum = Math.floor(((Math.random() % 3) * indoor.length));
+  var indexnum = Math.floor((Math.random()  * indoor.length));
   console.log(indoor[indexnum]);
   var link = document.getElementById('test')
   link.setAttribute("href", indoor[indexnum])
   // $("#plant-quote").text(indoor[indexnum]);
 })
+
+$("#test2").on("click", function() {
+  var outdoor = ["/test4/", "/test5/", "/test6/"]
+  var indexnum = Math.floor((Math.random()  * outdoor.length));
+  console.log(outdoor[indexnum]);
+  var link = document.getElementById('test2')
+  link.setAttribute("href", outdoor[indexnum])
+  // $("#plant-quote").text(indoor[indexnum]);
+})
+
 
 })
