ecb85b82051c0de9b5bd2ad723d406aa12a21b5c
sahil devnani <sahil.devnani@uleth.ca>
2022-10-31 20:56:45 -0600

    updated

--- Getactive/README.md
+++ Getactive/README.md
@@ -1,3 +1,6 @@
 # Get Active 
 Welcome to Get Active.
 The following app gives you new challenges to do.
+
+
+new plan would be from home button suprise button takes you --> Categories than under catogies add one more page for random shuffle of all 10 and make home page look better.
--- Getactive/www/js/scripts.js
+++ Getactive/www/js/scripts.js
@@ -257,18 +257,6 @@ var mainView = app.views.create('.view-main')
 
 $(document).on('page:init',function(e) {
 
-  /*
-function mixed() {
-  var mixed = ["/indoor1/", "/indoor2/", "/indoor3/", "/indoor4/", "/indoor5/", 
-                "/outdoor1/", "/outdoor2/", "/outdoor3/", "/outdoor4/", "/outdoor5/"]
-  var indexnum = Math.floor((Math.random()  * mixed.length));
-  console.log(mixed[indexnum]);
-  var link = document.getElementById('test3')
-  link.setAttribute("href", mixed[indexnum])
-}
-*/
-//mixed();
-
 function pickpage() {
   var indoor = ["/indoor1/", "/indoor2/", "/indoor3/", "/indoor4/", "/indoor5/"]
   var indexnum = Math.floor((Math.random()  * indoor.length));
